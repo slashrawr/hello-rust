ce1fe7534e2baea3fb404e99cdb02ce0d39f264d
slashrawr <brandon.michael.lewis@gmail.com>
2022-10-15 18:23:56 +0800

    Added functions.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::io;
 
-fn main() -> io::Result<()> {
+fn main() {
+    
+    let result = prompt();
+    let name = match result {
+        Ok(entered_name) => entered_name,
+        Err(error) => panic!("Error reading name: {:?}", error),
+    };
+    show(name);
+}
+
+fn prompt() -> Result<String, io::Error> {
     let mut input = String::new();
     println!("What is your name? ");
     io::stdin().read_line(&mut input)?;
-    println!("Hello, {}!", input.trim().to_string());
-    Ok(())
+    Ok(input)
+}
+
+fn show(name: String) {
+    println!("Hello, {}!", name.trim().to_string());
 }

0c9645500ac30074cf03553871df9eb17cfc8672
slashrawr <brandon.michael.lewis@gmail.com>
2022-10-15 19:21:53 +0800

    Added tests.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
 use std::io;
 
 fn main() {
-    
     let result = prompt();
     let name = match result {
         Ok(entered_name) => entered_name,
         Err(error) => panic!("Error reading name: {:?}", error),
     };
-    show(name);
+    println!("{}", build_output(name));
 }
 
 fn prompt() -> Result<String, io::Error> {
     let mut input = String::new();
     println!("What is your name? ");
     io::stdin().read_line(&mut input)?;
-    Ok(input)
+    Ok(input.trim().to_string())
 }
 
-fn show(name: String) {
-    println!("Hello, {}!", name.trim().to_string());
+fn build_output(name: String) -> String {
+    let output = format!("Hello {}!", name);
+    return output;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::build_output;
+
+    #[test]
+    fn formatted_output() {
+        let result  = build_output("Brandon".to_string());
+        assert!(result.contains("Hello Brandon!"))
+    }
 }

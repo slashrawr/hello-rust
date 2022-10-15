4f60af89646e889828b8aa078d17dcc4016b0969
slashrawr <brandon.michael.lewis@gmail.com>
2022-10-15 17:38:10 +0800

    Added input.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    println!("What is your name? ");
+    io::stdin().read_line(&mut input)?;
+    println!("Hello, {}!", input.trim().to_string());
+    Ok(())
 }
